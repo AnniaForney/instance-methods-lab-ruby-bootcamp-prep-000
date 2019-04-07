fa054f35091cba93167ecbd3206c5f8b06c40773
Annia Forney <anniaforney@gmail.com>
2019-04-07 01:48:21 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
-class Dog 
-  def bark 
-    puts "Woof!"
-end
+class Person
+  def talk
+    puts "Hello World!"
+  end
 
-def sit 
-  puts "The Dog is sitting"
-end 
+  def walk
+    puts "The Person is walking"
+  end
+end
